5be86a14c6646d4884e3c050f4f6b554ebcfcd08
arsalanahmed <ahmedarsalan030@gmail.com>
2024-09-24 11:42:18 +0500

    forms and user accounts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DetailView
-from .models import Post
+from django.views.generic import ListView , DetailView 
+from django.views.generic.edit import CreateView ,UpdateView ,DeleteView
+from .models import Post  # new
+from django.urls import reverse_lazy #
 # Create your views here.
 
 class BlogListView(ListView):
@@ -11,3 +13,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): # new
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = "post_delet.html"
+    success_url = reverse_lazy("home")
--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog.apps.BlogConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -118,8 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
+LOGOUT_REDIRECT_URL = "home"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "home" 
+LOGOUT_REDIRECT_URL = 'home' 
+
+
